48797bde55cacf2c363428699f8312f1564d0056
Fringie <fringie@live.co.uk>
2019-08-19 23:31:18 +0100

    final

--- setup cdn.ps1
+++ setup cdn.ps1
@@ -1,5 +1,6 @@
-﻿#https://www.c-sharpcorner.com/article/sharepoint-framework-deploy-spfx-webparts-to-office-365-public-cdn/Connect-SPOService -Url https://zaindev-admin.sharepoint.com 
+﻿#https://www.c-sharpcorner.com/article/sharepoint-framework-deploy-spfx-webparts-to-office-365-public-cdn/
+Connect-SPOService -Url https://zaindev-admin.sharepoint.com 
 Set-SPOTenantCdnEnabled -CdnType Public
 Add-SPOTenantCdnOrigin -CdnType Public -OriginUrl sites/cdn/cdn
 #"https://zaindev.sharepoint.com/sites/cdn/cdn/ExpenseClaimsWebPart"
-https://publiccdn.sharepointonline.com/zaindev.sharepoint.com/sites/cdn/cdn/ExpenseClaimsWebPart
+https://publiccdn.sharepointonline.com/zaindev.sharepoint.com/sites/cdn/cdn/expenseclaimswebpart
